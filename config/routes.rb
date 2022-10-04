d4915f3bc8f7464e920c456d867e73f45a9fc126
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-04 20:18:28 +0000

    [Add] 3-5 Listsコントローラへeditアクション（編集機能）を追加、editビューを作成、editアクションへのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   #.../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
