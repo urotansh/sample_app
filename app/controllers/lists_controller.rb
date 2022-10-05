947ef310930a00be37b5b7f76c0d2ead6784fecd
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-05 01:08:06 +0000

    [Add] 3-8 投稿機能の全カラムにバリデーションチェック（presence: true）を追加 、createアクションからnewビューへエラーメッセージを表示する条件分岐を追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,12 +6,13 @@ class ListsController < ApplicationController
   
   #以下を追加
   def create
-    #1.&2.データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    #3.データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    #4.詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      #:アクション名で、同じコントローラ内の別アクションのViewを表示
+      render :new
+    end
   end
 
   def index
